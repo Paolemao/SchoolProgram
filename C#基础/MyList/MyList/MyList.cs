2bb94609ac60eff030a921de352f75e282a651a3
Paolemao <33440840+Paolemao@users.noreply.github.com>
2017-11-16 19:41:26 +0800

    C#Base

---
+++ C#基础/MyList/MyList/MyList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class MyList<T>
+    {
+        T[] testArray;
+        int size;
+        public MyList()
+        {
+            size = 0;
+            testArray = new T[8];
+        }
+
+        public void Add(T testNum)
+        {
+            if (size==testArray.Length)
+            {
+                T[] temp = new T[testArray.Length * 2];
+                testArray.CopyTo(temp,0);
+                testArray = temp;
+            }
+            testArray[size] = testNum;
+            size++;
+        }
+        public void Remove(T testNum)
+        {
+            int index = -1;
+            for (int i = 0; i < size; i++)
+            {
+                if (testArray[i].Equals(testNum) )
+                {
+                    index = i;
+                }
+                RemoveAt(index);
+            }
+
+        }
+        public void RemoveAt(int remove_index)
+        {
+            int j = 0;
+            T[] temp2 = new T[testArray.Length];
+            testArray.CopyTo(temp2, 0);
+            for (int i=0;i<size;i++)
+            {
+                if (i==remove_index)
+                {
+                    j++;
+                    testArray[i] = testArray[i + j];
+                    size--;
+                }
+                testArray[i] = temp2[i +j];
+            }
+        }
+        public void Insert(int insert_index, T testNum)
+        {
+  
+            int j = 0;
+            T[] temp2 = new T[testArray.Length];
+            testArray.CopyTo(temp2, 0);
+            for (int i = 0; i < size; i++)
+            {
+
+                if (i == insert_index)
+                {
+                    testArray[i] = testNum;
+                    j++;
+                    size++;
+                }
+                else
+                {
+                    testArray[i] = temp2[i - j];
+                }
+            }
+        }
+        public T Get(int index)
+        {
+            return testArray[index];
+        }
+        public void Set(int index, T testNum)
+        {
+            testArray[index] = testNum;
+        }
+        public int Count()
+        {
+            return size;
+        }
+
+    }
+}
---
+++ C#基础/MyList/MyList/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> a = new MyList<int>();
+            a.Add(1);
+            a.Add(2);
+            a.Add(3);
+            a.Add(4);
+            a.Add(5);
+            a.Add(6);
+            a.Add(7);
+            a.Add(8);
+            a.Add(9);
+            a.Add(10);
+
+            a.Remove(10);
+            a.RemoveAt(5);
+            a.Insert(5,99);
+            for (int i = 0; i < a.Count(); ++i)
+            {
+                if (i == 5)
+                {
+                    Trace.Assert(a.Get(i) == 99);
+                }
+                else
+                {
+                    Trace.Assert(a.Get(i) == i + 1);
+                }
+                Console.WriteLine(a.Get(i));
+            }
+
+
+            MyList<int> l = new MyList<int>();
+            for (int i = 0; i <= 10000; ++i)
+            {
+                l.Add(i + 100);
+            }
+
+            for (int i = 100; i > 0; --i)
+            {
+                l.Insert(i * 100, 999);
+                l.RemoveAt(i * 100 - 1);
+            }
+
+            for (int i = 0; i < 10000; ++i)
+            {
+                int n = i + 100;
+                if (n % 100 == 99)
+                {
+                    Trace.Assert(l.Get(i) == 999);
+                }
+                else
+                {
+                    Trace.Assert(l.Get(i) == n);
+                }
+            }
+
+            Console.WriteLine("测试成功！");
+            Console.WriteLine("仅测试了Add, Insert, RemoveAt, Count四个方法");
+            Console.ReadKey();
+        }
+    }
+}
